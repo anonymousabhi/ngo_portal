e8707bcb51dafebe45d7d4ed02c8afa322a321a1
anonymousabhi <abhisheklal0095@gmail.com>
2019-04-28 18:00:07 +0530

    new

--- login.php
+++ login.php
@@ -6,10 +6,19 @@
 
 <?php include_once 'nav.php'; ?>
 
-
-
-
-
+<section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <a class="nav-link" href="welcome.php"><h2 class="display-4">login</h2></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
   <!-- Footer -->
---
+++ welcome.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<?php include_once 'head.php'; ?>
+</head>
+<body>
+
+
+
+<?php include_once 'welcomenav.php'; ?>
+
+
+
+
+
+<?php include_once 'footer.php' ; ?>
+
+<body>
+
+</html>
+
