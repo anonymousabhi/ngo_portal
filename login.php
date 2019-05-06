63f6c7b01bfe82b7cbe5235e2904c98d9f887521
anonymousabhi <abhisheklal0095@gmail.com>
2019-05-06 13:08:49 +0530

    bjj

--- login.php
+++ login.php
@@ -23,18 +23,17 @@
 
 
 <form action="action_page.php">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
+  
 
   <div class="container">
-    <label for="uname"><b>Username</b></label>
+    <label for="uname"><b>E-mail</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
     <br>
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
-
+    <br>
     <button type="submit">Login</button>
+    <br>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
