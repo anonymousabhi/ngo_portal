bdcf95be05419d2a704e3ec57f59ea278af79b38
Ankit Kumar <ankitkr0696@gmail.com>
2019-05-06 12:58:02 +0530

    Update child.php

--- child.php
+++ child.php
@@ -8,9 +8,9 @@
 
 
 
-Child labour refers to the exploitation of children through any form of work that deprives children of their childhood, interferes with their ability to attend regular school, and is mentally, physically, socially or morally harmful.[3] Such exploitation is prohibited by legislation worldwide,[4][5] although these laws do not consider all work by children as child labour; exceptions include work by child artists, family duties, supervised training, and some forms of child work practiced by Amish children, as well as by Indigenous children in the Americas.[6][7][8]
+Child labour refers to the exploitation of children through any form of work that deprives children of their childhood, interferes with their ability to attend regular school, and is mentally, physically, socially or morally harmful.[3] Such exploitation is prohibited by legislation worldwide,[4][5] although these laws do not consider all work by children as child labour; exceptions include work by child artists, family duties, supervised training, and some forms of child work practiced by Amish children, as well as by Indigenous children in the Americas.
 
-Child labour has existed to varying extents throughout history. During the 19th and early 20th centuries, many children aged 5–14 from poorer families worked in Western nations and their colonies alike. These children mainly worked in agriculture, home-based assembly operations, factories, mining, and services such as news boys—some worked night shifts lasting 12 hours. With the rise of household income, availability of schools and passage of child labour laws, the incidence rates of child labour fell.[9][10][11]
+Child labour has existed to varying extents throughout history. During the 19th and early 20th centuries, many children aged 5–14 from poorer families worked in Western nations and their colonies alike. These children mainly worked in agriculture, home-based assembly operations, factories, mining, and services such as news boys—some worked night shifts lasting 12 hours. With the rise of household income, availability of schools and passage of child labour laws, the incidence rates of child labour fell.
 
 
 
