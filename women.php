78ed8b73e46a9dd5cb0cdcc87ebec9401b56a0ab
anonymousabhi <abhisheklal0095@gmail.com>
2019-05-12 12:24:59 +0530

    Update women.php

--- women.php
+++ women.php
@@ -19,9 +19,9 @@
         <div class="col-lg-6 order-lg-1">
           <div class="p-5">
             <br>
-            <h2 class="display-4">CHILD LABOUR UPLIFTMENT</h2>
-            <p>Child labour refers to the exploitation of children through any form of work that deprives children of their childhood, interferes with their ability to attend regular school, and is mentally, physically, socially or morally harmful. Such exploitation is prohibited by legislation worldwide,although these laws do not consider all work by children as child labour; exceptions include work by child artists, family duties, supervised training, and some forms of child work practiced by Amish children, as well as by Indigenous children in the Americas.
-              Some of the Child NGO's are:-<br>
+            <h2 class="display-4">WOMEN EMPOWERMENT</h2>
+            <p>Women's empowerment is the process in which women elaborate and recreate what it is that they can be, do, and accomplish in a circumstance that they previously were denied.Empowerment can be defined in many ways, however, when talking about women's empowerment, empowerment means accepting and allowing people (women) who are on the outside of the decision-making process into it. “This puts a strong emphasis on participation in political structures and formal decision-making and, in the economic sphere, on the ability to obtain an income that enables participation in economic decision-making.” Empowerment is the process that creates power in individuals over their own lives, society, and in their communities. People are empowered when they are able to access the opportunities available to them without limitations and restrictions such as in education, profession and lifestyle. Feeling entitled to make your own decisions creates a sense of empowerment.
+              Some of the  NGO's are:-<br>
               <a href="ngo01.php">ABC of KOLKATA</a><br>
               <a href="#">xyz</a><br>
               <a href="#">abcd</a><br>
