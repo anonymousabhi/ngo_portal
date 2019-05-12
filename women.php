cb976b6006cb0425c704b7eca888de8aadc9b0c2
anonymousabhi <abhisheklal0095@gmail.com>
2019-05-12 12:41:38 +0530

    23

--- women.php
+++ women.php
@@ -22,7 +22,7 @@
             <h2 class="display-4">WOMEN EMPOWERMENT</h2>
             <p>Women's empowerment is the process in which women elaborate and recreate what it is that they can be, do, and accomplish in a circumstance that they previously were denied.Empowerment can be defined in many ways, however, when talking about women's empowerment, empowerment means accepting and allowing people (women) who are on the outside of the decision-making process into it. “This puts a strong emphasis on participation in political structures and formal decision-making and, in the economic sphere, on the ability to obtain an income that enables participation in economic decision-making.” Empowerment is the process that creates power in individuals over their own lives, society, and in their communities. People are empowered when they are able to access the opportunities available to them without limitations and restrictions such as in education, profession and lifestyle. Feeling entitled to make your own decisions creates a sense of empowerment.
               Some of the  NGO's are:-<br>
-              <a href="ngo01.php">ABC of KOLKATA</a><br>
+              <a href="women01.php">SEVA</a><br>
               <a href="#">xyz</a><br>
               <a href="#">abcd</a><br>
               <a href="#">India Liited</a><br>
