f85b4cfb7be627959eb603e24e597f8f821a14ee
anonymousabhi <abhisheklal0095@gmail.com>
2019-05-12 12:21:25 +0530

    new

---
+++ women.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<?php include_once 'head.php'; ?>
+</head>
+<body>
+
+<?php include_once 'nav.php'; ?>
+
+
+
+<section>
+    <div class="container mt-6">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="img/12.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <br>
+            <h2 class="display-4">CHILD LABOUR UPLIFTMENT</h2>
+            <p>Child labour refers to the exploitation of children through any form of work that deprives children of their childhood, interferes with their ability to attend regular school, and is mentally, physically, socially or morally harmful. Such exploitation is prohibited by legislation worldwide,although these laws do not consider all work by children as child labour; exceptions include work by child artists, family duties, supervised training, and some forms of child work practiced by Amish children, as well as by Indigenous children in the Americas.
+              Some of the Child NGO's are:-<br>
+              <a href="ngo01.php">ABC of KOLKATA</a><br>
+              <a href="#">xyz</a><br>
+              <a href="#">abcd</a><br>
+              <a href="#">India Liited</a><br>
+              <a href="#">XYZ</a><br>
+
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+<body>
+
+</html>
